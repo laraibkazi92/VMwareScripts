82a4422fcec9b1fa23e296d83d83e581d8fa20f8
Laraib Kazi <laraibkazi92@gmail.com>
2024-04-25 01:34:24 -0600

    Update nsxtVmcaCert.py
    Added updated API calls for newer NSX versions

--- nsxtVmcaCert.py
+++ nsxtVmcaCert.py
@@ -8,9 +8,18 @@ import argparse
 import sys
 import getpass
 import socket
+import logging
 from OpenSSL.crypto import load_certificate, FILETYPE_PEM
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+__author__ = 'Laraib Kazi'
+__version__ = '1.4.0'
+
+logdir = '/var/log/vmware/'
+logFile = logdir+'nsxtVmcaCert.log'
+logging.basicConfig( filename = logFile,filemode = 'a',level = logging.DEBUG,format = '%(asctime)s [%(levelname)s]: %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p' )
+logger = logging.getLogger(__name__)
 
 '''
 This script is to update NSX-T Node Certificates (Manager and VIP) to VMCA Signed Certificates.
@@ -29,14 +38,13 @@ The script has to be re-run for each NSX-T node.
 
 OPENSSL = '/usr/bin/openssl'
 
-__author__ = 'Laraib Kazi | @starlord'
-
 def prompt():
     '''
     Prompts for password for admin
     '''
     # Get password with no echo
     passwd = getpass.getpass("Provide password for the NSX-T admin user: ")
+    logger.info('admin password has been input.')
     return passwd
 
 def GetArgs():
@@ -58,6 +66,7 @@ def GetArgs():
 
 def openssl(*args):
     cmdline = [OPENSSL] + list(args)
+    logger.debug(f'Running command: {cmdline}')
     subprocess.check_call(cmdline,stderr=subprocess.DEVNULL,stdout=subprocess.DEVNULL)
 
 def createCsrConfigExt(fqdn):
@@ -144,27 +153,39 @@ def createCsrConfig(fqdn):
 }
     return csrConfig
 
-def createCsr(fqdn,user,passwd):
-    # API for CSR Request
-    
-    nsxtVersion = nsxtVersionChecker(fqdn,user,passwd)
-    
-    if nsxtVersion < 312:
-        csrConfig = createCsrConfig(fqdn)
-        api_url = "https://{}/api/v1/trust-management/csrs".format(fqdn)
-    else:
-        csrConfig = createCsrConfigExt(fqdn)
-        api_url = "https://{}/api/v1/trust-management/csrs-extended".format(fqdn)        
+def createCsr(fqdn,user,passwd,nsxtVersion):
+    # API for CSR Request 
+    try:
+        if nsxtVersion < 312:
+            logger.debug(f'NSX Version is < 312. Using legacy "csr" API.')
+            csrConfig = createCsrConfig(fqdn)
+            api_url = "https://{}/api/v1/trust-management/csrs".format(fqdn)
+        else:
+            logger.debug(f'NSX Version is > 312. Using new "csrs-extended" API.')
+            csrConfig = createCsrConfigExt(fqdn)
+            api_url = "https://{}/api/v1/trust-management/csrs-extended".format(fqdn)   
+    except Exception as e:
+        logger.error(f'Failed to create CSR Configuration. Error: {e}')   
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)  
 
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
+    logger.info(f'Attempting POST API Call with URL {api_url}')
     response = requests.request("POST", api_url, auth=(user,passwd), headers=headers, data=json.dumps(csrConfig), verify=False)
-    csr_text = json.loads(response.text)["pem_encoded"]
-    csr_id = json.loads(response.text)["id"]
-    #print(csr_text)
+    try:
+        csr_text = json.loads(response.text)["pem_encoded"]
+        csr_id = json.loads(response.text)["id"]
+        logger.debug(f'CSR generated successfully. Raw CSR:\n{csr_text}')
+        logger.debug(f'CSR ID:{csr_id}')
+    except Exception as e:
+        logger.error(f'Error generating CSR: {e}')
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)
 
     with open(fqdn+".csr", 'w') as f:
         f.write(csr_text)
+    logger.info(f'CSR written to file: {fqdn}.csr')
     
     print(" > CSR Created ... | CSR ID : "+csr_id)
     return csr_id
@@ -172,6 +193,7 @@ def createCsr(fqdn,user,passwd):
 def createVmcaSignedCert(fqdn,user,passwd):
     
     ipaddr = socket.gethostbyname(fqdn)
+    logger.info(f'IP Address for {fqdn}: {ipaddr}')
     
     OPENSSL_x509CONFIG = """subjectKeyIdentifier = hash
 authorityKeyIdentifier=keyid,issuer
@@ -183,28 +205,46 @@ subjectAltName = DNS: {}
 
     with open('extfile.conf', 'w') as f:
         f.write(OPENSSL_x509CONFIG)
+    logger.debug(f'openssl x509 config written to: "extfile.conf"')
 
     ca_cert_location = '/var/lib/vmware/vmca/root.cer'
     ca_key_location = '/var/lib/vmware/vmca/privatekey.pem'
     csr_location = fqdn+".csr"
 
-    openssl('x509', '-req', '-extfile', 'extfile.conf', '-days', '730', '-in', str(csr_location), '-CA', str(ca_cert_location), '-CAkey', str(ca_key_location), '-CAcreateserial', '-out', str(fqdn+".leaf.crt"), '-sha256')
-    
+    logger.info('Running openssl command to generate a VMCA signed certificate.')
+    try:
+        openssl('x509', '-req', '-extfile', 'extfile.conf', '-days', '730', '-in', str(csr_location), '-CA', str(ca_cert_location), '-CAkey', str(ca_key_location), '-CAcreateserial', '-out', str(fqdn+".leaf.crt"), '-sha256')
+        logger.info('VMCA Signed Certifcate created succesfully.')
+    except Exception as e:
+        logger.error(f'openssl command to create certificate failed. Error: {e}')
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)
+        
     # Concatentating VMCA cert to Server Certificate file
     with open(fqdn+".leaf.crt", 'r') as f1:
         server_content = f1.read()
+        logger.debug(f'Raw Certificate:\n{server_content}')
     with open("/var/lib/vmware/vmca/root.cer", 'r') as f2:
         ca_content = f2.read()
+    logger.debug('Combining the VMCA root and Leaf certificate to one file.')
     chain_content = server_content + ca_content
+    logger.debug(f'Raw Certificate Chain:\n{chain_content}')
     with open(fqdn+".crt", 'w') as f:
         f.write(chain_content)
+    logger.debug(f'Certificate Chain written to: {fqdn}.crt')
     
     print(" > VMCA Signed Certificate for {} created ...".format(fqdn))
 
 def importCertNSX(fqdn, csr_id,user,passwd):
     # Creating payload to import the certificate
-    with open(fqdn+".crt", 'r') as f:
-        cert_chain=f.read()
+    try:
+        with open(fqdn+".crt", 'r') as f:
+            cert_chain=f.read()
+        logger.debug(f'Reading certificate chain from file: {fqdn}.crt')
+    except Exception as e:
+        logger.error(f'Failed to read certificate chain from file. Error: {e}')
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)
 
     import_payload={ 
     "pem_encoded": cert_chain
@@ -215,25 +255,73 @@ def importCertNSX(fqdn, csr_id,user,passwd):
 
     headers = {'Content-Type': 'application/json'}
 
+    logger.info(f'Attempting POST API Call with URL {api_url}')
     response = requests.request("POST", api_url, auth=(user,passwd), headers=headers, data=json.dumps(import_payload), verify=False)
-    output = json.dumps(json.loads(response.text)["results"])
-    stripped_value=output.lstrip(output[0]).rstrip(output[-1])
-    crt_id = json.loads(stripped_value)["id"]
-
+    try:
+        output = json.dumps(json.loads(response.text)["results"])
+        stripped_value=output.lstrip(output[0]).rstrip(output[-1])
+        crt_id = json.loads(stripped_value)["id"]
+        logger.info(f'Certificate imported successfully to NSX Manager. Certificate ID: {crt_id}')
+        
+    except Exception as e:
+        logger.error(f'Failed to import certificate. Error: {e}')
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)
+    
     print(" > Certificate imported to {} ... | Certificate ID: {}".format(fqdn, crt_id))
     return crt_id
 
-def applyCertNsxtManager(fqdn,crt_id,user,passwd):
+def applyCertNsxtManager(fqdn,crt_id,user,passwd,nodeId):
     # Apply Certificate to NSX-T Manager
-    api_url = "https://{}/api/v1/node/services/http?action=apply_certificate&certificate_id={}".format(fqdn, crt_id)
+    api_url = f"https://{fqdn}/api/v1/trust-management/certificates/{crt_id}?action=apply_certificate&service_type=API&node_id={nodeId}"
+            
+    logger.info(f'Attempting POST API Call with URL {api_url}')
     response = requests.request("POST", api_url, auth=(user,passwd), verify=False)
-    print(" > Certificate applied to {} ...".format(fqdn))
+    if response.status_code == 200:
+        print(" > Certificate applied to {} ...".format(fqdn))
+        logger.info(f'Certificate applied successfully.')
+    else:
+        print(" !! Failed to apply certificate !!")
+        logger.info(f'Failed to apply certificate.')
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)
 
-def applyCertNsxtVIP(fqdn,crt_id,user,passwd):
+def applyCertNsxtVIP(fqdn,crt_id,user,passwd,nsxtVersion):
     # Apply Certificate to NSX-T VIP
-    api_url = "https://{}/api/v1/cluster/api-certificate?action=set_cluster_certificate&certificate_id={}".format(fqdn, crt_id)
+    if nsxtVersion < 320:
+        api_url = f"https://{fqdn}/api/v1/cluster/api-certificate?action=set_cluster_certificate&certificate_id={crt_id}"
+    else:
+        api_url = f"https://{fqdn}/api/v1/trust-management/certificates/{crt_id}?action=apply_certificate&service_type=MGMT_CLUSTER"
+        
+    logger.info(f'Attempting POST API Call with URL {api_url}')
     response = requests.request("POST", api_url, auth=(user,passwd), verify=False)
-    print(" > Certificate applied to {} ...".format(fqdn))
+    if response.status_code == 200:
+        print(" > Certificate applied to {} ...".format(fqdn))
+        logger.info(f'Certificate applied successfully.')
+    else:
+        print(" !! Failed to apply certificate !!")
+        logger.info(f'Failed to apply certificate.')
+        logger.error(f'Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.')
+        sys.exit(1)
+
+def getManagerNodeId(fqdn,user,passwd):
+    api_url = f"https://{fqdn}/api/v1/cluster"
+    logger.info(f'Attempting GET API Call with URL {api_url}')
+    response = requests.request("GET", api_url, auth=(user,passwd), verify=False)
+    cluster_info = response.json()
+    nodes = cluster_info["nodes"]
+    
+    required_nodeId = None
+    for node in nodes:
+        if fqdn == node["fqdn"]:
+            required_nodeId = node["node_uuid"]
+            logger.debug(f'Identified Node id for {fqdn}: {required_nodeId}')
+            break        
+    
+    if required_nodeId is None:
+        raise Exception
+    else:
+        return required_nodeId    
 
 def workflowValidator(fqdn):
     # Check if Certificate is applied
@@ -254,14 +342,18 @@ def workflowValidator(fqdn):
 def nsxtVersionChecker(fqdn,user,passwd):
     # Check NSX-T Version
     api_url = "https://{}/api/v1/node/version".format(fqdn)
+    logger.info(f'Attempting GET API Call with URL {api_url}')
     response = requests.request("GET", api_url, auth=(user,passwd), verify=False)
-    output = json.dumps(json.loads(response.text)["node_version"])
-    print(" > NSX-T Version Detected {} ...".format(output))
+    try:
+        output = json.dumps(json.loads(response.text)["node_version"])
+        logger.debug(f'NSX Version from API: {output}')
+        print(" > NSX-T Version Detected {} ...".format(output))
+        shortOutput = output.split(".")
+        shortVersion = int(shortOutput[0].replace('"','')+shortOutput[1]+shortOutput[2])
+        return shortVersion
+    except Exception as e:
+        logger.error(f'Failed to get NSX Version. Error: {e}')
     
-    shortOutput = output.split(".")
-    shortVersion = int(shortOutput[0].replace('"','')+shortOutput[1]+shortOutput[2])
-    return shortVersion
-
 def main():
 
     args = GetArgs()
@@ -270,14 +362,28 @@ def main():
     fqdn = args.fqdn
     user = 'admin'
 
-    csrId = createCsr(fqdn,user,passwd)
+    # In case the API to get NSX Version fails
+    # We give it a default value of 4.0.0
+    try:
+        nsxtVersion = nsxtVersionChecker(fqdn,user,passwd)
+    except:
+        nsxtVersion = 400
+    logger.debug(f'NSX Version assigned: {nsxtVersion}')
+    
+    
+    try:
+        nodeId = getManagerNodeId(fqdn,user,passwd)
+    except:
+        logger.error("Failed to get node id. Please review the logs at /var/log/vmware/nsxtVmcaCert.log for details.")
+        
+    csrId = createCsr(fqdn,user,passwd,nsxtVersion)
     createVmcaSignedCert(fqdn,user,passwd)
     crtId = importCertNSX(fqdn,csrId,user,passwd)
     
     if args.manager:
-        applyCertNsxtManager(fqdn,crtId,user,passwd)
+        applyCertNsxtManager(fqdn,crtId,user,passwd,nodeId)
     elif args.vip:
-        applyCertNsxtVIP(fqdn,crtId,user,passwd)
+        applyCertNsxtVIP(fqdn,crtId,user,passwd,nsxtVersion)
     else:
         print("No arguements specified.")
     
